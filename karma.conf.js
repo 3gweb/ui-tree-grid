128d5217996229af8fba5a6cdf562099ede942ce
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2014-07-30 19:11:48 -0300

    adding travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,72 +1,77 @@
 // Karma configuration
 // Generated on Wed Jul 30 2014 16:15:43 GMT-0300 (BRT)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+	config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-		'bower_components/angular/angular.js',
-		'bower_components/angular-resource/angular-resource.js',
-	    'bower_components/angular-mocks/angular-mocks.js',
-	    'src/*.js',
-	    'src/**/*.js',
-	    'src/templates/*.html',
-		'test/**/*.js'
-    ],
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+		// list of files / patterns to load in the browser
+		files: [
+			'bower_components/angular/angular.js',
+			'bower_components/angular-resource/angular-resource.js',
+			'bower_components/angular-mocks/angular-mocks.js',
+			'src/*.js',
+			'src/**/*.js',
+			'src/templates/*.html',
+			'test/**/*.js'
+		],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-	    "src/**/*.js": "coverage"
-    },
+		// list of files to exclude
+		exclude: [
+		],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','coverage'],
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			"src/**/*.js": "coverage"
+		},
 
 
-    // web server port
-    port: 9876,
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress', 'coverage'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+		// web server port
+		port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['PhantomJS'],
+
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
+
+		plugins: [
+			'karma-phantomjs-launcher',
+			'karma-jasmine'
+		]
+	});
 };
